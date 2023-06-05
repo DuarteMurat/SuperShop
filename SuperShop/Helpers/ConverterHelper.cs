441cd4713ac8a9c98a2911a98a35b0e23ed6ba9a
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-06-05 17:46:54 +0100

    ImageHelper and ConverterHelper

---
+++ SuperShop/Helpers/ConverterHelper.cs
@@ -0,0 +1,42 @@
+﻿using SuperShop.Data.Entities;
+using SuperShop.Models;
+
+namespace SuperShop.Helpers
+{
+    public class ConverterHelper : IConverterHelper
+    {
+
+        public Product ToProduct(ProductsViewModel model, string path, bool isNew)
+        {
+            return new Product
+            {
+                Id = isNew ? 0 : model.Id,
+                ImageUrl = path,
+                IsAvilable = model.IsAvilable,
+                LastPurchase = model.LastPurchase,
+                LastSale = model.LastSale,
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock,
+                User = model.User,
+            };
+        }
+
+        public ProductsViewModel ToProductViewModel(Product product)
+        {
+            return new ProductsViewModel
+            {
+                Id = product.Id,
+                IsAvilable = product.IsAvilable,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                ImageUrl = product.ImageUrl,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                User = product.User,
+            };
+        }
+
+    }
+}
---
+++ SuperShop/Helpers/IConverterHelper.cs
@@ -0,0 +1,12 @@
+﻿using SuperShop.Data.Entities;
+using SuperShop.Models;
+
+namespace SuperShop.Helpers
+{
+    public interface IConverterHelper
+    {
+        Product ToProduct(ProductsViewModel model,string path, bool isNew); 
+
+        ProductsViewModel ToProductViewModel (Product product);
+    }
+}
