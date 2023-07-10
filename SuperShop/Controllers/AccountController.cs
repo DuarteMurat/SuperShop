cd14a90a672aa29377e3b9ce017cdcb4d897b2b9
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-07-10 14:52:49 +0100

    Redirect pages

--- SuperShop/Controllers/AccountController.cs
+++ SuperShop/Controllers/AccountController.cs
@@ -171,5 +171,10 @@ namespace SuperShop.Controllers
 
             return this.View(model);
         }
+
+        public IActionResult NotAuthorized()
+        {
+            return View();
+        }
     }
 }
