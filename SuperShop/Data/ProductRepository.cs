23e60beeb47f8187f43e99e5866e6ea615c1bc18
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-07-27 09:47:29 +0100

    Products combo

--- SuperShop/Data/ProductRepository.cs
+++ SuperShop/Data/ProductRepository.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace SuperShop.Data
 {
     public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
-        private readonly DataContext _context; 
+        private readonly DataContext _context;
         public ProductRepository(DataContext context) : base(context)
         {
             _context = context;
@@ -16,6 +19,22 @@ namespace SuperShop.Data
         {
             return _context.Products.Include(p => p.User);
         }
+
+        public IEnumerable<SelectListItem> GetComboProducts()
+        {
+            var list = _context.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.Id.ToString(),
+            }).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a product...)",
+                Value = "0",
+            });
+
+            return list;
+        }
     }
 }
- 
