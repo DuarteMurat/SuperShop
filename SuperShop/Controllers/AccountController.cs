c19652c13233dc4c2fd3a54a34133859e936b986
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-08-13 00:40:55 +0100

    Create tokens

--- SuperShop/Controllers/AccountController.cs
+++ SuperShop/Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SuperShop.Data;
 using SuperShop.Data.Entities;
 using SuperShop.Helpers;
 using SuperShop.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SuperShop.Controllers
@@ -13,18 +19,21 @@ namespace SuperShop.Controllers
     {
         private readonly IUserHelper _userHelper;
         private readonly ICountryRepository _countryRepository;
+        private readonly IConfiguration _configuration;
 
         public AccountController(
             IUserHelper userHelper,
+            IConfiguration configuration,
             ICountryRepository countryRepository)
         {
             _userHelper = userHelper;
             _countryRepository = countryRepository;
+            _configuration = configuration;
         }
 
         public IActionResult Login()
         {
-            if(User.Identity.IsAuthenticated)
+            if (User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
             }
@@ -80,9 +89,9 @@ namespace SuperShop.Controllers
                 {
                     var city = await _countryRepository.GetCityAsync(model.CityId);
 
-                    user = new User 
-                    { 
-                        FirstName = model.FirstName, 
+                    user = new User
+                    {
+                        FirstName = model.FirstName,
                         LastName = model.LastName,
                         Email = model.Username,
                         UserName = model.Username,
@@ -93,7 +102,7 @@ namespace SuperShop.Controllers
                     };
 
                     var result = await _userHelper.AddUserAsync(user, model.Password);
-                    if(result != IdentityResult.Success)
+                    if (result != IdentityResult.Success)
                     {
                         ModelState.AddModelError(string.Empty, "The user couldn't be created.");
                         return View(model);
@@ -107,7 +116,7 @@ namespace SuperShop.Controllers
                     };
 
                     var result2 = await _userHelper.LoginAsync(loginViewModel);
-                    if(result2.Succeeded)
+                    if (result2.Succeeded)
                     {
                         return RedirectToAction("Index", "Home");
                     }
@@ -211,6 +220,50 @@ namespace SuperShop.Controllers
             }
 
             return this.View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUSerByEmailAsync(model.Username);
+                if (user != null)
+                {
+                    var result = await _userHelper.ValidatePasswordAsync(
+                        user,
+                        model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+                        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            _configuration["Tokens:Issuer"],
+                            _configuration["Tokens:Audience"],
+                            claims,
+                            expires: DateTime.UtcNow.AddDays(15),
+                            signingCredentials: credentials);
+                        var results = new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        };
+
+                        return this.Created(string.Empty, results);
+
+                    }
+                }
+            }
+
+            return BadRequest();
         }
 
         public IActionResult NotAuthorized()
